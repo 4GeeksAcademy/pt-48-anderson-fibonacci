614026db43b34af0b6113697a9353bd1c7e75de7
AndyGH86 <141663667+AndyGH86@users.noreply.github.com>
2023-10-10 10:14:16 +0200

    fibonacci listo

--- fibonacci.js
+++ fibonacci.js
@@ -1,9 +1,20 @@
-document.querySelector('#button-addon1').addEventListener("click", () => {
-const longitudFib = document.querySelector ("#longitud");
+const buttonGenerateFib = document.getElementById("generateFib");
+const inputNumber = document.getElementById("inputNumber");
+const resultNumbers = document.getElementById("resultNumbers");
+
+buttonGenerateFib.addEventListener("click", () => {
+  const n = parseInt(inputNumber.value);
+  const newFibonacciNumber = fib(n);
+  resultNumbers.innerHTML = "";
+  newFibonacciNumber.forEach((num) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = num;
+    resultNumbers.appendChild(listItem);
+  });
 });
 
-const imprimirFib = (n, numFibInicio = [0, 1]) => {
-   if (n <= numFibInicio.length) {
+const fib = (n, numFibInicio = [0, 1]) => {
+  if (n <= numFibInicio.length) {
     return numFibInicio;
   }
 
@@ -11,10 +22,5 @@ const imprimirFib = (n, numFibInicio = [0, 1]) => {
     numFibInicio[numFibInicio.length - 1] +
     numFibInicio[numFibInicio.length - 2];
   numFibInicio.push(nextValue);
-  return imprimirFib(n, numFibInicio);
+  return fib(n, numFibInicio);
 };
-
-const newFibonacci = document.getElementById("respuesta");
-newFibonacci = imprimirFib(longitudFib);
-console.log(newFibonacci);
-
